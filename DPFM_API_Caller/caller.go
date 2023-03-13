70ca73e0c04eaa3cb84a7405c2535857364ecf36
MiyuYokoyama0109 <miyu.y@latona.io>
2023-03-13 14:05:03 +0900

    release

--- DPFM_API_Caller/caller.go
+++ DPFM_API_Caller/caller.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	dpfm_api_input_reader "data-platform-api-payment-terms-exconf-rmq-kube/DPFM_API_Input_Reader"
 	dpfm_api_output_formatter "data-platform-api-payment-terms-exconf-rmq-kube/DPFM_API_Output_Formatter"
-	"data-platform-api-payment-terms-exconf-rmq-kube/database"
-	"sync"
+	"encoding/json"
 
 	"github.com/latonaio/golang-logging-library-for-data-platform/logger"
+	database "github.com/latonaio/golang-mysql-network-connector"
+	rabbitmq "github.com/latonaio/rabbitmq-golang-client-for-data-platform"
+	"golang.org/x/xerrors"
 )
 
 type ExistenceConf struct {
@@ -24,61 +26,56 @@ func NewExistenceConf(ctx context.Context, db *database.Mysql, l *logger.Logger)
 	}
 }
 
-func (e *ExistenceConf) Conf(input *dpfm_api_input_reader.SDC) *dpfm_api_output_formatter.PaymentTerms {
-	paymentTerms := *input.PaymentTerms.PaymentTerms
-	notKeyExistence := make([]string, 0, 1)
-	KeyExistence := make([]string, 0, 1)
+func (e *ExistenceConf) Conf(msg rabbitmq.RabbitmqMessage) interface{} {
+	var ret interface{}
+	ret = map[string]interface{}{
+		"ExistenceConf": false,
+	}
+	input := make(map[string]interface{})
+	err := json.Unmarshal(msg.Raw(), &input)
+	if err != nil {
+		return ret
+	}
 
-	existData := &dpfm_api_output_formatter.PaymentTerms{
-		PaymentTerms:      paymentTerms,
+	_, ok := input["PaymentTerms"]
+	if ok {
+		input := &dpfm_api_input_reader.SDC{}
+		err = json.Unmarshal(msg.Raw(), input)
+		ret = e.confPaymentTerms(input)
+		goto endProcess
+	}
+
+	err = xerrors.Errorf("can not get exconf check target")
+endProcess:
+	if err != nil {
+		e.l.Error(err)
+	}
+	return ret
+}
+
+func (e *ExistenceConf) confPaymentTerms(input *dpfm_api_input_reader.SDC) *dpfm_api_output_formatter.PaymentTerms {
+	exconf := dpfm_api_output_formatter.PaymentTerms{
+		ExistenceConf: false,
+	}
+	if input.PaymentTerms.PaymentTerms == nil {
+		return &exconf
+	}
+	exconf = dpfm_api_output_formatter.PaymentTerms{
+		PaymentTerms:  *input.PaymentTerms.PaymentTerms,
 		ExistenceConf: false,
 	}
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		if !e.confPaymentTerms(paymentTerms) {
-			notKeyExistence = append(notKeyExistence, paymentTerms)
-			return
-		}
-		KeyExistence = append(KeyExistence, paymentTerms)
-	}()
-
-	wg.Wait()
-
-	if len(KeyExistence) == 0 {
-		return existData
-	}
-	if len(notKeyExistence) > 0 {
-		return existData
-	}
-
-	existData.ExistenceConf = true
-	return existData
-}
-
-func (e *ExistenceConf) confPaymentTerms(val string) bool {
 	rows, err := e.db.Query(
 		`SELECT PaymentTerms 
 		FROM DataPlatformMastersAndTransactionsMysqlKube.data_platform_payment_terms_payment_terms_data 
-		WHERE PaymentTerms = ?;`, val,
+		WHERE PaymentTerms = ?;`, exconf.PaymentTerms,
 	)
 	if err != nil {
 		e.l.Error(err)
-		return false
+		return &exconf
 	}
+	defer rows.Close()
 
-	for rows.Next() {
-		var paymentTerms string
-		err := rows.Scan(&paymentTerms)
-		if err != nil {
-			e.l.Error(err)
-			continue
-		}
-		if paymentTerms == val {
-			return true
-		}
-	}
-	return false
+	exconf.ExistenceConf = rows.Next()
+	return &exconf
 }
